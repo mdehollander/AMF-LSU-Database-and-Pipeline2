1d2a87af4b55c70ae4b429fd432d5b40156f97ef
Mattias <m.dehollander@nioo.knaw.nl>
2026-09-09 11:16:49 +0200

    Add missing commands

--- 2024_AMFPipeline/2024_AMFPipeline_ASV/AMFrenameASVs.sh
+++ 2024_AMFPipeline/2024_AMFPipeline_ASV/AMFrenameASVs.sh
@@ -1,8 +1,9 @@
 # WIP, paths needs to be adjusted
+# It takes the ASV_seqs.fasta and ASV_table.tsv output from ampliseq stored in the dada2 directory
 # This is a workaround method. If downstream processing scripts don't need the explicit ASV naming, this step becomes obsolete
 
 # Add numbered ASVs to the fasta header, keeping the original hash
-awk '/^>/{printf ">ASV_" ++i "_" substr($0,2) "\n"; next} 1' ../out-maxee/dada2/ASV_seqs.fasta  > q2files/ASV_seqs.fasta
+awk '/^>/{printf ">ASV_" ++i "_" substr($0,2) "\n"; next} 1' ../../../ampliseq_out/dada2/ASV_seqs.fasta  > q2files/ASV_seqs.fasta
 
 # Also adjust the ASV_ID column in the table. Not the best approach, a second cut command is needed. But it works:
-awk 'NR==1 {print "ASV_ID\t" $0; next} {print "ASV_" NR-1 "_" $1 "\t" $0}' ../out-maxee/dada2/ASV_table.tsv | cut --complement -f2 > q2files/ASV_table.tsv
+awk 'NR==1 {print "ASV_ID\t" $0; next} {print "ASV_" NR-1 "_" $1 "\t" $0}' ../../../ampliseq_out/dada2/ASV_table.tsv | cut --complement -f2 > q2files/ASV_table.tsv
--- 2024_AMFPipeline/2024_AMFPipeline_ASV/README.md
+++ 2024_AMFPipeline/2024_AMFPipeline_ASV/README.md
@@ -5,18 +5,33 @@ export C_ENV="amf_pipeline"
 
 pixi exec nextflow run nf-core/ampliseq -r 2.15.0 -params-file nf-params.json --skip_dada_taxonomy
 
+mkdir q2files
+
 bash AMFrenameASVs.sh
 
-bash AMFtrimmedToASV.sh
+bash AMFtrimmedToASVs.sh
+
+export R1cutoff=170
+export R2cutoff=140
 
 bash AMFalignseqs.sh 
 
 bash AMFlaunchTrees.sh
 
+# Clean the tree with gappa
+
+pixi exec gappa prepare clean-tree --tree-file Root_V18_LSUDB_052025.newick --remove-comments-and-nhx     
+
+mkdir AMFtree.ENVbackbone
 pixi exec epa-ng --tree clean-tree.newick --ref-msa ./V18_LSUDB_052025_cut_aligned.fasta --query ./BLAST_ASVrepseqs_clean_aligned.fasta --model GTR+G --threads 80 --outdir AMFtree.ENVbackbone --redo
 
 pixi exec gappa examine graft --jplace-path AMFtree.ENVbackbone/epa_result.jplace --allow-file-overwriting
 
+mkdir ./RAxMLfiles/
+cd RAxMLfiles/
+ln -s ../epa_result.newick RAxML_bestTree.newick
+cd ..
+mkdir cladeOutputs/
 bash AMFcladesExtract.sh
 
 bash AMFcladeTaxonomy.sh
